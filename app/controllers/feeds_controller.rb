f4d790cf64facd06195120fc66d8d8e19fad1865
v.kovalevskiy <ikik1@yandex.ru>
2014-10-23 08:43:52 +0300

    Fixed warnings

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -7,17 +7,17 @@ class FeedsController < ApplicationController
   def feed
 
     #todo проверить списки (только для глобал одинаковые)
-    queryPrivacy = params.require(:global);
+    query_privacy = params.require(:global)
     
-    feeds = nil;
+    feeds = nil
 
-    case queryPrivacy
+    case query_privacy
     when 0
-      feeds = get_global_feed();
+      feeds = get_global_feed
     when 1
-      feeds = get_friends_feed();
+      feeds = get_friends_feed
     when 2
-      feeds = get_private_feed();
+      feeds = get_private_feed
     else
     end
 
@@ -29,20 +29,18 @@ class FeedsController < ApplicationController
 
   #use will_paginate gem
   def get_global_feed
-    feed = FeedsHelper::get_feed_message_format(Feed.where("privacy = 0 AND status != 0").includes(:from_profile, :to_profile).order(id: :desc).first(100))
-    return feed  
+    feed = FeedsHelper::get_feed_message_format(Feed.where('privacy = 0 AND status != 0').includes(:from_profile, :to_profile).order(id: :desc).first(100))
   end
 
   #hard-nailed solution
   def get_friends_feed    
     #get friends list    
     ids = $user.get_friends_id
-    feed = FeedsHelper::get_feed_message_format(Feed.where("privacy = 0 AND status != 0 AND (to_profile_id in (:ids) OR from_profile_id in (:ids))", ids: ids).includes(:from_profile, :to_profile).order(id: :desc).first(100))    
-    return feed;
+    feed = FeedsHelper::get_feed_message_format(Feed.where('privacy = 0 AND status != 0 AND (to_profile_id in (:ids) OR from_profile_id in (:ids))', ids: ids).includes(:from_profile, :to_profile).order(id: :desc).first(100))
   end
 
   def get_private_feed()
-    feed = FeedsHelper::get_feed_message_format(Feed.where("to_profile_id = :id OR from_profile_id = :id", id: $user.id).includes(:from_profile, :to_profile).order(status: :asc, id: :desc).first(100))  
+    feed = FeedsHelper::get_feed_message_format(Feed.where('to_profile_id = :id OR from_profile_id = :id', id: $user.id).includes(:from_profile, :to_profile).order(status: :asc, id: :desc).first(100))
   end
 
 end
