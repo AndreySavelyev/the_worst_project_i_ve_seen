3c011430bf1a123569b4ca65b11a102a1a8cf029
Mikhail Zelenin <mikhail.zelenin@gmail.com>
2014-12-18 13:13:02 +0200

    * new staging wallet. restore correct migration name.

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -12,11 +12,12 @@ class CallbackController < ApplicationController
     logger.debug "CallbackController :: New order received: #{order_xml} : with hash: #{ext_hash}"
 
     my_hash_old = calc_hash(order_xml,'cqgL1Fw82K3Y')
-    my_hash_new = calc_hash(order_xml,'8vWJR3Xs0uw9')
+    my_hash_new_production = calc_hash(order_xml,'8vWJR3Xs0uw9')
+    my_hash_new_staging = calc_hash(order_xml,'lLCo39SpYb15')
 
-    logger.debug "CallbackController :: Hash calculated: old #{my_hash_old} : new: #{my_hash_new}"
+    logger.debug "CallbackController :: Hash calculated: old #{my_hash_old} : new_production: #{my_hash_new_production} : new_staging: #{my_hash_new_staging}"
 
-    if my_hash_old == ext_hash || my_hash_new == ext_hash
+    if my_hash_old == ext_hash || my_hash_new_production == ext_hash || my_hash_new_staging == ext_hash
       h = CallbackHelper.parse_callback(order_xml)
 
       token = Rails.env == 'development' ? '0d0e8674fc76aae2a587ba4c591ebd36' : h['order']['number']
