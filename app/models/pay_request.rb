ac83cb363f7c7c9efdd7e0ba9d780377ceb61ab8
Mikhail Zelenin <mikhail.zelenin@gmail.com>
2015-01-13 17:04:59 +0200

    * logging

--- app/models/pay_request.rb
+++ app/models/pay_request.rb
@@ -54,6 +54,13 @@ class PayRequest < Feed
 
     send_request.set_commission(currency)
 
+    logger.debug "create_accepted_request :: send_request.check_balance && Limit::check(currency, send_request.from_profile) :: " + (!!send_request.check_balance).to_s + ' ' + (!!Limit::check(currency, send_request.from_profile)).to_s
+    logger.debug "create_accepted_request :: self.from_profile.wallet.available  self.commission_amount.to_f self.conv_commission_amount self.source_amount :: " +
+                     self.from_profile.wallet.available.to_s + ' ' +
+                     self.commission_amount.to_f.to_s + ' ' +
+                     self.conv_commission_amount.to_s + ' ' +
+                     self.source_amount.to_s
+
     if send_request.check_balance && Limit::check(currency, send_request.from_profile)
       send_request.save!
 
